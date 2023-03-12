1958120f9e7ddfdfdcf12ddb0ce35b319ddd2fe9
Petr Vecera <petr.vecera@profiq.com>
2023-03-12 22:26:41 +0100

    Changes to the mixpanel

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -4,24 +4,40 @@ import {getVersion} from "@tauri-apps/api/app";
 mixpanel.init("bf92acb0810b9e7d4a49e63efc41433d",  {debug: true})
 
 const clientId = "test";
+let version = ""
 
+/**
+ * The events for Mixpanel
+ * Make sure that init is called before any other event
+ */
 const events = {
     init: async (): Promise<void> => {
+
+        version = await getVersion()
+
         mixpanel.track("app_init", {
             distinct_id: clientId,
-            version: await getVersion(),
+            version,
         })
     },
-    open_about: async (): Promise<void> => {
+    open_about: (): void => {
         mixpanel.track("open_about", {
             distinct_id: clientId,
-            version: await getVersion(),
+            version,
         })
     },
-    open_settings: async (): Promise<void> => {
+    open_settings: (): void => {
         mixpanel.track("open_settings", {
             distinct_id: clientId,
-            version: await getVersion(),
+            version,
+        })
+    },
+    settings_changed: (setting: string, value?: string | number): void => {
+        mixpanel.track("settings_changed", {
+            distinct_id: clientId,
+            version,
+            setting,
+            value
         })
     }
 }
