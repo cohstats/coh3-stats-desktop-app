6e070c97dd354136e4caacea0ec50880abb60f08
Petr Vecera <petr.vecera@profiq.com>
2023-06-29 00:06:19 +0200

    Add online players

---
+++ src/utils/steam-api.ts
@@ -0,0 +1,12 @@
+const baseUrl = "https://api.steampowered.com/"
+const COH3_STEAM_APP_ID = 1677280
+
+const getNumberOfOnlinePlayersSteamUrl = (
+  appId: number | string = COH3_STEAM_APP_ID
+) => {
+  return encodeURI(
+    `${baseUrl}ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=${appId}`
+  )
+}
+
+export { getNumberOfOnlinePlayersSteamUrl }
