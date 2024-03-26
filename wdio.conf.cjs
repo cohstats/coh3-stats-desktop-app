a52998546044136026dd2916bd0e53d5067b9522
Petr Vecera <vecera.petr@gmail.com>
2024-03-26 22:45:53 +0100

    change the path

--- wdio.conf.cjs
+++ wdio.conf.cjs
@@ -12,7 +12,7 @@ exports.config = {
     {
       maxInstances: 1,
       'tauri:options': {
-        application: './src-tauri/target/release/coh3-stats-desktop-app.exe',        
+        application: './src-tauri/target/release/Coh3 Stats Desktop App.exe',        
         webviewOptions: {},
       },
     },
